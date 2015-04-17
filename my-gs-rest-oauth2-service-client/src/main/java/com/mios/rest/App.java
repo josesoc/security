344facfee081fc80068353e3be89c8193d402a86
josesoc <josesocialnetworks@gmail.com>
2015-04-17 13:06:43 +0200

    Notes about Http request token

--- my-gs-rest-oauth2-service-client/src/main/java/com/mios/rest/App.java
+++ my-gs-rest-oauth2-service-client/src/main/java/com/mios/rest/App.java
@@ -21,6 +21,52 @@ public class App {
 		//get();
 	}
 	
+	/**
+	 * HTTP Request a Token: 
+	 * 
+	 *    $ curl -X POST -vu clientapp:123456 http://localhost:6000/oauth/token 
+	 *           -H "Accept: application/json" 
+	 *           -d "password=12345678&username=pepe&grant_type=password&scope=read%20write&client_secret=123456&client_id=clientapp"
+	 * 
+			POST /oauth/token HTTP/1.1
+			Authorization: Basic Y2xpZW50YXBwOjEyMzQ1Ng==
+			User-Agent: curl/7.33.0
+			Host: 127.0.0.1:6000
+			Accept: application/json
+			Content-Length: 111
+			Content-Type: application/x-www-form-urlencoded
+
+			password=12345678&username=pepe&grant_type=password&scope=read%20write&client_secret=123456&client_id=clientapp
+			
+			
+			HTTP/1.1 200 OK
+			Server: Apache-Coyote/1.1
+			X-Content-Type-Options: nosniff
+			X-XSS-Protection: 1; mode=block
+			Cache-Control: no-cache, no-store, max-age=0, must-revalidate
+			Pragma: no-cache
+			Expires: 0
+			X-Frame-Options: DENY
+			X-Application-Context: application:hsql:9090
+			Cache-Control: no-store
+			Pragma: no-cache
+			Content-Type: application/json;charset=UTF-8
+			Transfer-Encoding: chunked
+			Date: Fri, 17 Apr 2015 09:59:30 GMT
+
+			cc
+			{
+  				"access_token" : "9f653980-3b2a-4bb6-aa87-afd4ad2c4114",
+  				"token_type" : "bearer",
+  				"refresh_token" : "63fd2425-1ceb-4947-9f9e-2b5a41b02b33",
+  				"expires_in" : 43199,
+  			"scope" : "read write"
+			}
+			0		
+		
+
+	 * @throws Exception
+	 */
 	public static void getToken() throws Exception {
 		String tokenLocationURL="http://localhost:8080/oauth/token";
 		String username="pepe";
